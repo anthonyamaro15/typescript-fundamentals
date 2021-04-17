97aff1b11b233e6dc94f36caaf45203af360b648
Anthony Amaro <aamaro@bradmins-MacBook-Pro.local>
2021-04-16 18:25:57 -0700

    feat(app): initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+   const t = (value: any) => {
+      console.log(value);
+   };
+   return (
+      <div className="App">
+         <h1>TypeScript Basics</h1>
+      </div>
+   );
+}
+
+export default App;
